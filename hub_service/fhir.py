df2f31f422d87764ef37478b91ee7dcbffb8b3af
Omar Richardson <omarr@simula.no>
2024-08-27 14:01:27 +0200

    Remove auth-lib and openssl binary dependency

--- hub_service/fhir.py
+++ hub_service/fhir.py
@@ -16,7 +16,7 @@ from dataclasses import dataclass
 import requests
 from urllib.parse import urlencode
 import jwt
-from authlib.jose import jwk
+
 
 prefix = os.environ.get("JUPYTERHUB_SERVICE_PREFIX", "/")
 auth = HubOAuth(api_token=os.environ["JUPYTERHUB_API_TOKEN"], cache_max_age=60)
@@ -46,8 +46,8 @@ class OAuthState:
 
 def get_jwks(key_file: str = "jwtRS256.key", key_id: str = "1") -> str:
     try:
-        with open(key_file + ".pub", "rb") as f:
-            pem_public_key = f.read()
+        with open(key_file + ".pub", "r") as f:
+            public_key = f.read()
     except FileNotFoundError:
         print(f"Public key file {key_file}.pub not found. Generating new key pair")
 
@@ -56,20 +56,37 @@ def get_jwks(key_file: str = "jwtRS256.key", key_id: str = "1") -> str:
         subprocess.call(
             ["openssl", "rsa", "-in", key_file, "-pubout", "-out", f"{key_file}.pub"]
         )
-
-        # Read the newly generated public key
+        subprocess.call(
+            [
+                "ssh-keygen",
+                "-t",
+                "rsa",
+                "-b",
+                "4096",
+                "-m",
+                "PEM",
+                "-f",
+                key_file,
+                "-q",
+                "-N",
+                "",
+            ]
+        )
         with open(key_file + ".pub", "rb") as f:
-            pem_public_key = f.read()
+            public_key = f.read()
 
-    # Create JWKS and store in SMART-compliant format
-    jwks = jwk.dumps(pem_public_key, kty="RSA")
-    jwks["kid"] = key_id
-    jwks["alg"] = "RS256"
-    jwks_smart = {"keys": [jwks]}
+    alg = jwt.get_algorithm_by_name("RS256")
+    key = alg.prepare_key(public_key)
+    jwk = alg.to_jwk(key, as_dict=True)
+    jwk.update({"alg": "RS256", "kid": key_id})
+    jwks_smart = {"keys": [jwk]}
     jwks_smart_str = json.dumps(jwks_smart, indent=2)
     # Printing the JWKS to console to include in the SMART-on-FHIR launch
     print(jwks_smart_str)
     return jwks_smart_str
+
+
+get_jwks()
 
 
 def generate_jwt(key_file: str = "jwtRS256.key", key_id: str = "1") -> str:
@@ -138,7 +155,6 @@ def authenticated(f):
         else:
             # redirect to login url on failed auth
             session["smart_config"] = get_smart_config()
-            get_jwks()
             state = generate_state(next_url=request.path)
             from_redirect = make_response(start_oauth_flow(state_id=state.state_id))
             from_redirect.set_cookie(
