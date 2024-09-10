f09fe368884b17dcec1222bd37421fad3c4f057b
Omar Richardson <omarr@simula.no>
2024-09-10 18:44:27 +0200

    Load sandbox as fixture from specific folder

---
+++ tests/conftest.py
@@ -0,0 +1,29 @@
+import pytest
+import os
+import subprocess
+import requests
+import time
+
+
+@pytest.fixture(scope="function")  # module?
+def sandbox():
+    port = 5555
+    os.environ["PORT"] = str(port)
+    a = subprocess.Popen(
+        ["npm", "run", "start:prod"],
+        cwd=os.environ.get("SANDBOX_DIR", ".")
+    )
+    url = f"http://localhost:{port}"
+
+    # Wait until the frontend is ready
+    for _ in range(10):
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                break
+        except requests.ConnectionError:
+            pass
+        time.sleep(1)  # Wait for 1 second before retrying
+
+    yield url
+    a.kill()
